c6b9b419979cf08ae1256aefcca5906fb78e2f67
Rucyvelt <elthrasherrucyv@gmail.com>
2026-07-21 21:27:28 +0300

    Fix include path to look one level up for includes folder

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/includes/functions.php';
+require_once __DIR__ . '/../includes/functions.php';
 ss_session_start();
 require_login('/login.php');
 require_role(['super_admin', 'admin'], '/dashboard.php');
